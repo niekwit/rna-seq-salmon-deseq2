717b5f418063f5e7c8c92a8e199ca8fb42231a31
niekwit <41302859+niekwit@users.noreply.github.com>
2023-10-27 15:36:15 +0100

    added conditional color palette

--- workflow/scripts/pca.R
+++ workflow/scripts/pca.R
@@ -14,16 +14,26 @@ load(snakemake@input[[1]])
 # log transform data
 rld <- rlog(dds)
 
+# select appropriate colour palette
+if (length(unique(rld$treatment)) == 2) {
+  palette <- "Paired"
+} else {
+  palette <- "Dark2"
+}
+
 #create PCA plot
 pca <- plotPCA(rld, intgroup=c("genotype", "treatment")) +
-  geom_label_repel(aes(label = name)) + 
+  geom_label_repel(aes(label = rld$sample)) + 
   guides(colour = "none") +
   theme_bw() +
-  theme(text=element_text(size=14)) +
-  scale_color_brewer(palette = "Set1")
+  theme(text=element_text(size = 16)) +
+  scale_color_brewer(palette = palette)
 
 # save plot to file
-ggsave(snakemake@output[[1]], pca)
+ggsave(snakemake@output[[1]], 
+       pca, 
+       width=10,
+       height=10)
 
 
 # close redirection of output/messages
