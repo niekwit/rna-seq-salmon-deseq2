2245d98584cfb8d6884730c5668f52953f8f7686
niekwit <41302859+niekwit@users.noreply.github.com>
2023-10-27 15:34:58 +0100

    merged functions

--- workflow/scripts/general_functions.py
+++ workflow/scripts/general_functions.py
@@ -1,13 +1,23 @@
 import pandas as pd
 import sys
+import glob
+import os
 
 def import_samples():
     try:
         csv = pd.read_csv("config/samples.csv")
         SAMPLES = csv["sample"]
         
-        return SAMPLES
+        # check if samples from samples.csv match fastq files (both R1 and R2) in reads folder
+        for sample in SAMPLES:
+            r1 = f"reads/{sample}_R1.fastq.gz"
+            r2 = f"reads/{sample}_R2.fastq.gz"
+            
+            assert any(os.path.isfile(x) for x in [r1,r2]), f"ERROR: one or more fastq files (R1/R2) from sample {sample} not found in reads directory"
 
+        return SAMPLES
+        
     except FileNotFoundError:
-        print("ERROR: config/samples.csv not found!")
+        print("ERROR: config/samples.csv not found")
         sys.exit(1)
+        
