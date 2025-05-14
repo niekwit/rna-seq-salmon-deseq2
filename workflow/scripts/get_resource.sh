6421479fbd66b02458b5d4b6514cac50d2fa90cc
niekwit <41302859+niekwit@users.noreply.github.com>
2025-05-14 10:22:53 +0100

    files to log

--- workflow/scripts/get_resource.sh
+++ workflow/scripts/get_resource.sh
@@ -4,5 +4,6 @@ LOG=${snakemake_log[0]}
 URL=${snakemake_params["url"]}
 OUTPUT=${snakemake_output[0]}
 
-wget -q $URL -O $OUTPUT.gz 2> $LOG
+echo "Downloading/decompressing $URL to $OUTPUT" > $LOG
+wget -q $URL -O $OUTPUT.gz 2>> $LOG
 pigz -df $OUTPUT.gz 2>> $LOG
